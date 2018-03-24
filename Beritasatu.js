19494b01ea7f25fe875067e5a490b5ddcc96c413
Firmanriawan <30766350+Firmanriawan@users.noreply.github.com>
2018-03-24 09:47:33 +0700

    Create Beritasatu.js

---
+++ Beritasatu.js
@@ -0,0 +1,1 @@
+document.write( '<iframe id=\"video_target\" src=\"http://www.beritasatu.com/playstream.php?channel=news\" width=\"100%\" height=\"600\" frameborder=\"0\" allowfullscreen></iframe>\n' );
