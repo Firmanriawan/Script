995bb7de11ba3e1a0b8d601d4c9e2e2fec6ed7a2
Firmanriawan <30766350+Firmanriawan@users.noreply.github.com>
2018-03-24 09:54:13 +0700

    Create Mangastv.js

---
+++ Mangastv.js
@@ -0,0 +1,1 @@
+document.write( '<iframe frameborder=\"0\"width=\"560px\"height=\"315px\"style=\" width: 560px; height: 315px;\"src=\"http://www.streamgaroo.com/streams/394/667\"allowfullscreen></iframe>' );
