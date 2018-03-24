931330a107deea51fc52617ee5c794d602c9dd97
Firmanriawan <30766350+Firmanriawan@users.noreply.github.com>
2018-03-24 09:50:44 +0700

    Create Foxnews.js

---
+++ Foxnews.js
@@ -0,0 +1,1 @@
+document.write( '<iframe frameborder=\"0\"width=\"560px\"height=\"315px\"style=\" width: 560px; height: 315px;\"src=\"https://vaughnlive.tv/embed/video/newzviewz?viewers=false&autoplay=true&H=375\"allowfullscreen></iframe>' );
