037566aea37d052db40d5097a1f434288bb59801
Firmanriawan <30766350+Firmanriawan@users.noreply.github.com>
2018-03-24 09:53:17 +0700

    Create Intv.js

---
+++ Intv.js
@@ -0,0 +1,2 @@
+document.write( '<iframe id=\"MivoIframe\" height=\"420\" width=\"720\" allowfullscreen=\"\" src=\"https://www.mivo.com/embed/?v=https://edge.nim.mivo.tv/intv6vkhr543hkwt/intv_all/playlist.m3u8\"></iframe>\n' );
+document.write( '<!--<iframe id=\"MivoIframe\" height=\"420\" width=\"720\" allowfullscreen=\"\" src=\"https://player-cdn.mivo.tv/partner/intv.html\"></iframe>-->\n' );
